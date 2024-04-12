e0f8d952d540cf48dcb9e7cb3205193a5f1d6e86
Sandeep Poudel <poudel.sandeep@cloco.co.jp>
2024-04-12 11:08:23 +0900

    Search.txt file changed

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,9 +1,17 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+import { useSearchParams } from 'next/navigation';
 export default function Search({ placeholder }: { placeholder: string }) {
+    const searchParams = useSearchParams();
+
     function handleSearch(term: string) {
+        const params = new URLSearchParams(searchParams);
+        if (term) {
+            params.set('query', term);
+        } else {
+            params.delete('query');
+        }
         console.log(term);
     }
 
